7847572f0b9d2cd9d062855daebfd38bd2fd7f2b
BL-Starlord <1021618717@qq.com>
2024-03-17 11:04:15 +0000

    Hashmap

---
+++ Topic_3_Hashmap/Q242.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        from collections import defaultdict
+        
+        s_dict = defaultdict(int)
+        t_dict = defaultdict(int)
+        for x in s:
+            s_dict[x] += 1
+        
+        for x in t:
+            t_dict[x] += 1
+        return s_dict == t_dict
