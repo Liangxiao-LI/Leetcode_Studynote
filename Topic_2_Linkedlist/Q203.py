6a816d11a626deda0db3aa279832ef28fa76026e
BL-Starlord <1021618717@qq.com>
2024-03-17 11:00:36 +0000

    Create Q203.py

---
+++ Topic_2_Linkedlist/Q203.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        
+        # 创建虚拟头部节点以简化删除过程
+        dummy_head = ListNode(next = head)
+
+        # 遍历列表并删除值为val的节点
+        current = dummy_head
+        
+        while current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        
+        return dummy_head.next
